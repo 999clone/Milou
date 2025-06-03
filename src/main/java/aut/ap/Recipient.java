63872bcaa311b053ebc324ef8464d0d3c2d8e08c
shervin <shervinrajabi2016@gmail.com>
2025-06-03 19:55:14 +0330

    add Recipient class to connect emails and users.

---
+++ src/main/java/aut/ap/Recipient.java
@@ -0,0 +1,63 @@
+package aut.ap;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "recipients")
+public class Recipient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "email_id")
+    private Email email;
+
+    @Column(name = "is_read", nullable = false)
+    private boolean isRead = false;
+
+    public Recipient() {}
+
+    public Recipient(User user, Email email) {
+        this.user = user;
+        this.email = email;
+        this.isRead = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public void setEmail(Email email) {
+        this.email = email;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean read) {
+        isRead = read;
+    }
+}
